4deed7a23ab6070a73bcc70917db0849a9481488
AMN-47 <akhilnagar57@gmail.com>
2026-03-17 17:07:34 +0200

    Coded tests for calclator function

--- practice.test.js
+++ practice.test.js
@@ -20,3 +20,18 @@ test("A reverseString function that takes a string and returns it reversed.", ()
     A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. 
     Each of these functions should take two numbers and return the correct calculation.
 */
+test("A calculator object that contains functions for the basic operations: ADD", () => {
+    expect(practice.calculator(1,1)).toBe(1);
+});
+
+test("A calculator object that contains functions for the basic operations: SUBTRACT", () => {
+    expect(practice.calculator(2,1)).toBe(1);
+});
+
+test("A calculator object that contains functions for the basic operations: MULTIPLY", () => {
+    expect(practice.calculator(2,3)).toBe(6);
+});
+
+test("A calculator object that contains functions for the basic operations: DIVIDE", () => {
+    expect(practice.calculator(10,2)).toBe(5);
+});
