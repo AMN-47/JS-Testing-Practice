2e495f5c8e7194aaaeaf6563811962b744726914
AMN-47 <akhilnagar57@gmail.com>
2026-03-18 19:27:03 +0200

    Added reverseString function and tested it

--- practice.js
+++ practice.js
@@ -2,4 +2,9 @@ function capitalize(str) {
     return str = (str.charAt(0)).toUpperCase() + str.slice(1);
 } 
 
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
 module.exports.capitalize = capitalize;
+module.exports.reverseString = reverseString;
